298c39d656fa0ea09b01bcdcf457e12fb349e0a7
Jakub Kraus <90748640+JakubKraus2@users.noreply.github.com>
2022-04-11 12:28:40 +0100

    dynamic port function, new port and PlayerProjectile texture

--- src/Data/Score.gd
+++ src/Data/Score.gd
@@ -1,8 +1,14 @@
 extends Node
 
 signal score_updated
+signal level_updated
 var score: = 0 setget set_score
+var level = 1 setget set_level
 
 func set_score(value: int) -> void:
 	score = value
+	emit_signal("score_updated")
+
+func set_level(value: int) -> void:
+	level = value
 	emit_signal("score_updated")
--- src/Objects/Port.gd
+++ src/Objects/Port.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
 
+
 func _on_Port_body_entered(body):
-	get_tree().change_scene("res://src/Levels/Level_test2.tscn")
+	get_tree().change_scene("res://src/Levels/Level_test" + str(Score.level + 1) + ".tscn")
+	Score.level += 1
+
